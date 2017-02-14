2381331eae004a24ad31c372d6d7343555ea085a
Valentina Aleksieva <martin@Martins-MacBook-Pro.local>
2017-02-14 22:23:50 +0100

    Valentina’s implementations

---
+++ src/se/kth/carInspection/integration/InspectorDTO.java
@@ -0,0 +1,31 @@
+package com.company;
+/**
+ * Write a description of class InspectorDTO here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class InspectorDTO
+{
+    private String name;
+    private String inspectionCompany;
+
+    /**
+     * Constructor for objects of class InspectorDTO
+     */
+    public InspectorDTO(String name, String inspectionCompany)
+    {
+        this.name = name;
+        this.inspectionCompany = inspectionCompany;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+    
+    public String getInspectionCompany()
+    {
+        return inspectionCompany;
+    }
+}
